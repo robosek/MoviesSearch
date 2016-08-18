f396a32546be294462efb940319d6e15cbde70b1
Robert Sęk <Robert.Sek@dfc.local>
2016-08-18 10:33:37 +0200

    Changes: error message, Added: not found image and movie type category

--- js/controllers/movieController.js
+++ js/controllers/movieController.js
@@ -1,25 +1,32 @@
-define(["modules/app","services/movieHttpFacade"],function(app,movieHttpFacade){
-   app.controller("movieController",["$scope","movieHttpFacade",function($scope,movieHttpFacade){
-    
-       $scope.search = function(title, isValid){
-           
-           if(isValid)
-               {
-                    $scope.error = false;
-            movieHttpFacade.getSampleMovie(title).success(function(data){
-        
-        $scope.movie = data;
-            if(data.Response == "False"){
-                $scope.error = true;
+define(["modules/app", "services/movieHttpFacade"], function (app, movieHttpFacade) {
+    app.controller("movieController", ["$scope", "movieHttpFacade", function ($scope, movieHttpFacade) {
+
+        var noImageAddress = "http://www.novelupdates.com/img/noimagefound.jpg";
+
+        $scope.search = function (title, isValid) {
+
+            if (isValid) {
+                delete $scope.movie;
+                $scope.error = false;
+                movieHttpFacade.getSampleMovie(title).success(function (data) {
+
+                    $scope.movie = data;
+                    if (data.Response == "False") {
+                        $scope.error = true;
+                    }
+                    else {
+                        $scope.image = (data.Poster === "N/A" ? noImageAddress : data.Poster);
+                    }
+
+
+                }).error(function (error) {
+                    $scope.error = true;
+                });
+
             }
-        }).error(function(error){
-            $scope.error = true;
-        });
-                   
+
         }
-                
-       }
-       
-   }]);
-    
+
+    }]);
+
 });
