ef3b57572c6c06c98fb702991f54446fd52d92f8
Robert Sęk <robosek@robosek.pl>
2016-08-23 18:26:10 +0200

    Call https instead of http imdb api service

--- js/services/movieHttpFacade.js
+++ js/services/movieHttpFacade.js
@@ -1,11 +1,11 @@
 define(["modules/app"],function(app){
     app.factory("movieHttpFacade",["$http",function($http){
         var _getSampleMovie = function(title){
-            return $http.get("http://www.omdbapi.com/?t="+title+"&y=&plot=full&r=json")
+            return $http.get("https://www.omdbapi.com/?t="+title+"&y=&plot=full&r=json")
         };
 
         var _getMovieById = function (id) {
-            return $http.get("http://www.omdbapi.com/?i="+id+"&y=&plot=full&r=json")
+            return $http.get("https://www.omdbapi.com/?i="+id+"&y=&plot=full&r=json")
         }
         
         return{
