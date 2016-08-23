86a7e4d9c6cc4eff095c7c63f9e2f19a676d5f8b
Robert Sęk <robosek@robosek.pl>
2016-08-23 18:56:27 +0200

    Removed tests and main.js file for deploy

--- spec/hello_test.spec.js
+++
@@ -1,8 +0,0 @@
-define([],function(){
-    describe("It's just simple test to check if jasmine is prepared properly",function(){
-    it("It should return 4",function(){
-       expect(2+2).toBe(4);
-    });
-});
-});
-
