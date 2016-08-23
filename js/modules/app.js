f1ab783b30bcee5317560a333822cc01cad95dd6
Robert Sęk <robosek@robosek.pl>
2016-08-23 18:50:09 +0200

    Clean solution to deploy

--- js/modules/app.js
+++
@@ -1,7 +0,0 @@
-define(["angular","loadingBar","route","angucomplete"],function(angular){
-   var app = angular.module("myApp",["angular-loading-bar","ngRoute","angucomplete-alt"]);
-    app.init = function(){
-      angular.bootstrap(document,["myApp"]);  
-    };
-    return app;
-});
